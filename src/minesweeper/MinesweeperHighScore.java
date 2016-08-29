2a2456ca4014e2e63680cf28a43d05ea4ee5396c
Richard Song <brsong22@gmail.com>
2016-08-28 21:54:37 -0500

    high score methods
    add functionality to high score class

--- src/minesweeper/MinesweeperHighScore.java
+++ src/minesweeper/MinesweeperHighScore.java
@@ -1,5 +1,35 @@
 package minesweeper;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MinesweeperHighScore {
 
+	private int highscore;
+	private String filePath = "/minesweeper/src/minesweeper/hiscore.txt";
+	
+	public MinesweeperHighScore(){
+		File f = new File(filePath);
+		if(f.exists()) { 
+		    try {
+		    	String txtScore;
+				BufferedReader b = new BufferedReader(new FileReader(f));
+				try {
+					txtScore = b.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else{
+			
+		}
+	}
 }
